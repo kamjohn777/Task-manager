f0337c406c27d17ee47d57692bef2c4f2637517d
kamjohn777 <kamjohn79.kdj@gmail.com>
2023-09-20 14:35:04 -0500

    did an npm install and it added some dependencies and files

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Update/PUT
+router.put('/', function(req, res, next) {
+
+});
+
 module.exports = router;
